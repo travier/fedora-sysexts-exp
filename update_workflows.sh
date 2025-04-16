f9d26a1bfe40a88786ee7b63c0cda8c015ed3558
Timothée Ravier <tim@siosm.fr>
2025-04-16 12:42:18 +0200

    wip

--- update_workflows.sh
+++ update_workflows.sh
@@ -37,36 +37,32 @@ generate() {
     # 'quay.io/fedora-ostree-desktops/kinoite:41'
     # 'quay.io/fedora-ostree-desktops/kinoite:42'
 
+    # Set jobnames
+    declare -A jobnames
+    jobnames["quay.io/fedora-ostree-desktops/base-atomic:41"]="fedora-41"
+    jobnames["quay.io/fedora-ostree-desktops/base-atomic:42"]="fedora-42"
+    jobnames["quay.io/fedora/fedora-coreos:stable"]="fedora-coreos-stable"
+    jobnames["quay.io/fedora/fedora-coreos:next"]="fedora-coreos-next"
+
+    # Get the list of sysexts for each target
+    declare -A sysexts
     for image in "${images[@]}"; do
-        # Get the list of sysexts for a given target
-        sysexts=()
+        list=()
         for s in $(git ls-tree -d --name-only HEAD | grep -Ev ".github|.workflow-templates"); do
             pushd "${s}" > /dev/null
             # Only require the architecture to be explicitly listed for non x86_64 for now
             if [[ "${arch}" == "x86_64" ]]; then
                 if [[ $(just targets | grep -c "${image}") == "1" ]]; then
-                    sysexts+=("${s}")
+                    list+=("${s}")
                 fi
             else
                 if [[ $(just targets | grep -cE "${image} .*${arch}.*") == "1" ]]; then
-                    sysexts+=("${s}")
+                    list+=("${s}")
                 fi
             fi
             popd > /dev/null
         done
-
-        if [[ "${image}" == "quay.io/fedora-ostree-desktops/base-atomic:41" ]]; then
-            sysexts_fedora_41=("${sysexts[@]}")
-        fi
-        if [[ "${image}" == "quay.io/fedora-ostree-desktops/base-atomic:42" ]]; then
-            sysexts_fedora_42=("${sysexts[@]}")
-        fi
-        if [[ "${image}" == "quay.io/fedora/fedora-coreos:stable" ]]; then
-            sysexts_fedora_coreos_stable=("${sysexts[@]}")
-        fi
-        if [[ "${image}" == "quay.io/fedora/fedora-coreos:next" ]]; then
-            sysexts_fedora_coreos_next=("${sysexts[@]}")
-        fi
+        sysexts["${image}"]="$(echo "${list[@]}" | tr ' ' ';')"
     done
 
     local -r tmpl=".workflow-templates/"
@@ -87,53 +83,22 @@ generate() {
         "${tmpl}/00_sysexts_header"
 
     for image in "${images[@]}"; do
-        jobname=""
-        if [[ "${image}" == "quay.io/fedora-ostree-desktops/base-atomic:41" ]]; then
-            jobname="fedora-41"
-            sysexts=("${sysexts_fedora_41[@]}")
-        fi
-        if [[ "${image}" == "quay.io/fedora-ostree-desktops/base-atomic:42" ]]; then
-            jobname="fedora-42"
-            sysexts=("${sysexts_fedora_42[@]}")
-        fi
-        if [[ "${image}" == "quay.io/fedora/fedora-coreos:stable" ]]; then
-            jobname="fedora-coreos-stable"
-            sysexts=("${sysexts_fedora_coreos_stable[@]}")
-        fi
-        if [[ "${image}" == "quay.io/fedora/fedora-coreos:next" ]]; then
-            jobname="fedora-coreos-next"
-            sysexts=("${sysexts_fedora_coreos_next[@]}")
-        fi
-
-        sed -e "s|%%JOBNAME%%|${jobname}|g" \
+        sed -e "s|%%JOBNAME%%|${jobnames["${image}"]}|g" \
             -e "s|%%IMAGE%%|${image}|g" \
             "${tmpl}/10_sysexts_build_header"
         echo ""
-        for s in "${sysexts[@]}"; do
+        for s in $(echo "${sysexts["${image}"]}" | tr ';' ' '); do
             sed "s|%%SYSEXT%%|${s}|g" "${tmpl}/15_sysexts_build"
             echo ""
         done
     done
 
-    # TODO
+    # TODO: Dynamic list of jobs to depend on
     cat "${tmpl}/20_sysexts_gather_header"
 
     for image in "${images[@]}"; do
-        if [[ "${image}" == "quay.io/fedora-ostree-desktops/base-atomic:41" ]]; then
-            sysexts=("${sysexts_fedora_41[@]}")
-        fi
-        if [[ "${image}" == "quay.io/fedora-ostree-desktops/base-atomic:42" ]]; then
-            sysexts=("${sysexts_fedora_42[@]}")
-        fi
-        if [[ "${image}" == "quay.io/fedora/fedora-coreos:stable" ]]; then
-            sysexts=("${sysexts_fedora_coreos_stable[@]}")
-        fi
-        if [[ "${image}" == "quay.io/fedora/fedora-coreos:next" ]]; then
-            sysexts=("${sysexts_fedora_coreos_next[@]}")
-        fi
-
         echo ""
-        for s in "${sysexts[@]}"; do
+        for s in $(echo "${sysexts["${image}"]}" | tr ';' ' '); do
             sed -e "s|%%SYSEXT%%|${s}|g" "${tmpl}/25_sysexts_gather"
             echo ""
         done
